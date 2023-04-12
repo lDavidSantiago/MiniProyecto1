dc17b1ed7108160ef483017fca8ab8dd9ee3f8f5
lDavidSantiago <david.triana@correounivalle.edu.co>
2023-04-12 15:24:25 -0500

    Update Main

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-class JavaFuntions {
+class Main {
     public static void main(String[] args) {
-        System.out.println("My First Java Program.");
+        System.out.println("Cristian le gusta el yupi");
     }
 };
