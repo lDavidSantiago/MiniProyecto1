ebe950b606cc8e4d4e8ba71f3732545061ffdf8f
lDavidSantiago <david.triana@correounivalle.edu.co>
2023-04-12 16:18:09 -0500

    Pet and Raza

--- Pet.java
+++ Pet.java
@@ -1,3 +1,30 @@
 public class Pet {
+    private boolean vaccine;
+    private String origin_country;
+    private float pet_price;
+
+    public boolean isVaccine() {
+        return vaccine;
+    }
+
+    public void setVaccine(boolean vaccine) {
+        this.vaccine = vaccine;
+    }
+
+    public float getPet_price() {
+        return pet_price;
+    }
+
+    public void setPet_price(float pet_price) {
+        this.pet_price = pet_price;
+    }
+
+    public String getOrigin_country() {
+        return origin_country;
+    }
+
+    public void setOrigin_country(String origin_country) {
+        this.origin_country = origin_country;
+    }
 
 }
---
+++ Raza.java
@@ -0,0 +1,3 @@
+public enum Raza {
+    Pug, Labrador, Tacita, Pastor, Schnauzer, Salchicha
+}
