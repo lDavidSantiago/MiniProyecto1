cd23552e2859970a60e263fc3c79df4fc4ea498a
Ganzolitario98 <106501604+Ganzolitario98@users.noreply.github.com>
2023-04-13 12:22:04 -0500

    Se crea la funcion menuFuncional

--- Menuu.java
+++ Menuu.java
@@ -7,19 +7,41 @@ public class Menuu {
     public Menuu(){
         
     }
-    public static void clearScreen() {
-
+    public void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
      
     }
-    public void mostrarMenu(){
-
+    public void menuFuncional(){//6
+        byte opcion;
+        clearScreen();
         System.out.println("===VETERINARIA PATITAS==");
-        System.out.println("\nBienvenido, estamos para servirle.\n");
-        System.out.println("1. Insertar mascota\n2. Actualizar mascota\n3. Eliminar mascota\n4. Buscar mascota por nombre\n5. Listar todas las mascotas");
-        System.out.println("Seleccione una opcion: ");
+        System.out.println("Bienvenido al menu de funciones.");
+        System.out.println("1. Cuantas mascotas tienen la vacuna malota?\n2. Top 5 mascotas más costosas\n3. Que mascotas no tienen país de origen en latinoamérica?\n4. Finalizar programa");
+        System.out.print("Seleccione una opcion: ");
         opcion = scanner.nextByte();
+        switch(opcion){
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3: 
+                break;
+            case 4:
+                break;
+            
+        }
+
+    }
+    public void mostrarMenu(){
+        clearScreen();
+        System.out.println("===VETERINARIA PATITAS==");
+        System.out.println("\nMENU DE GESTION");
+        System.out.println("\nBienvenido, estamos para servirle.\n");
+        System.out.println("1. Insertar mascota\n2. Actualizar mascota\n3. Eliminar mascota\n4. Buscar mascota por nombre\n5. Listar todas las mascotas\n6. Menu de opciones funcionales");
+        System.out.print("Seleccione una opcion: ");
+        opcion = scanner.nextByte();
+        
         switch (opcion) {
             //manejar recursividad para siempre al finalizar volver al menu.
             
@@ -39,7 +61,10 @@ public class Menuu {
 
                 break;
             
-        
+            
+            case 6:
+                menuFuncional();
+                break;  
             default:
                 clearScreen();
 
