b10bc77bd18256208449223cca098f072569d1ea
William4954t <williamfrancootero@gmail.com>
2023-04-25 21:38:29 -0500

    mejoras muchachos

--- ArrayObject.java
+++ ArrayObject.java
@@ -15,49 +15,62 @@ public class ArrayObject {
         }
     }
     public void insert_pets(){
+        System.out.print("Su mascota es un perro o un gato? Gato = 1 | Perro = 2: ");
+        byte identifier = input.nextByte();
+        clearScreen();
+        input.nextLine();
         System.out.print("Nombre: ");
         String originName = input.nextLine();
-        System.out.print("Raza: ");
+        System.out.print("Raza: ");             
         String razeName = input.nextLine();
         System.out.print("Vacuna Malota: ");
         Boolean vaccineValue = input.nextBoolean();
         System.out.print("Origen: ");
         input.nextLine();
         String originValue = input.nextLine();
-        System.out.println("Precio: ");
+        System.out.print("Precio: ");
         float priceValue = input.nextFloat();
-        System.out.println("Su mascota es un perro o un gato? Gato = 1 | Perro = 2");
-        byte identifier = input.nextByte();
+
         if(identifier==1){
             Cat n = new Cat(originName,razeName,vaccineValue,originValue,priceValue,"Gato");
             arrayOfPets.add(n);
         }
         else if(identifier==2){
-            Cat n = new Cat(originName,razeName,vaccineValue,originValue,priceValue,"Perro");
+            Dog n = new Dog(originName,razeName,vaccineValue,originValue,priceValue,"Perro");
             arrayOfPets.add(n);
 
         }
+        ////////////////////////////////////////////////////////////////////////////////////////////////
+        if(identifier==1){
+            System.out.println("Su gato ha sido introducido al menui correctamente :D");
+            input.nextLine();
+        }
+        else if(identifier==2){
+            System.out.println("Su perro ha sido introducido al menui correctamente :D");
+            input.nextLine();
+        }
         input.nextLine();
+
 
     }
     //PUNTO 2
     public void change_pets_values(){
-        System.out.println("ingrese el nombre del animal que quiere realizar cambios: ");
+        System.out.print("ingrese el nombre del animal que quiere realizar cambios: ");
         String temp_name = input.nextLine();
         int index = 0;
         for (Pet pet: arrayOfPets){
             String xd = pet.pet_name;
                 if(temp_name.equals(xd)){
-                    System.out.println("Nombre: ");
+                    System.out.print("Nombre: ");
                     String newName = input.nextLine();
-                    System.out.println("Raza: ");
+                    System.out.print("Raza: ");
                     String newrazeName = input.nextLine();
-                    System.out.println("Vacuna Malota: ");
+                    System.out.print("Vacuna Malota: ");
                     Boolean newvaccineValue = input.nextBoolean();
-                    System.out.println("Origen: ");
+                    System.out.print("Origen: ");
                     input.nextLine();
                     String neworiginValue = input.nextLine();
-                    System.out.println("Precio: ");
+                    System.out.print("Precio: ");
                     float newpriceValue = input.nextFloat();
                         //Remplazar los valores
                         arrayOfPets.get(index).setPet_name(newName);
@@ -71,7 +84,7 @@ public class ArrayObject {
         }
         //PUNTO 3
         public void remove_mascota(){
-            System.out.println("ingrese el nombre del animal que quiere eliminar: ");
+            System.out.print("ingrese el nombre del animal que quiere eliminar: ");
             String temp_name = input.nextLine();
             for (Pet pet: arrayOfPets){
                 String xd = pet.pet_name;
@@ -85,7 +98,7 @@ public class ArrayObject {
 
         //PUNTO 4
         public void test(){
-            System.out.println("ingrese el nombre del animal que quiere buscar: ");
+            System.out.print("ingrese el nombre del animal que quiere buscar: ");
             String temp_name = input.nextLine();
             int index = 0;
             for (Pet pet: arrayOfPets){
